d554817ffb4cc67abd39457e0b50195010445254
char1eschen <martian2005@gmail.com>
2018-08-27 00:09:26 +1000

    finish moneypage

---
+++ app/js/money/index.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div>
+      <Heador/>  
+      <Slider/>
+      <Ad1/>
+      <Money/>
+      <Ad2/>
+      <Finance/>
+      <Ad3/>
+      <Footor cname="mfooter"/>
+      <Navbar/> 
+  </div>
+</template>
+
+<script>
+  import Heador from '../public/header.vue'
+  import Footor from '../public/footer.vue'
+  import Navbar from '../public/navbar.vue'
+  import Slider from './mslider.vue' 
+  import Ad1 from './ad1.vue'
+  import Money from './money.vue'
+  import Ad2 from './ad2.vue'
+  import Finance from './finance.vue'
+  import Ad3 from './ad3.vue'
+  export default {
+    components: {
+      Heador,
+      Slider,
+      Ad1,
+      Money,
+      Ad2,
+      Finance,
+      Ad3,
+      Footor,
+      Navbar
+    }
+  }
+</script>
+
+<style lang="scss">
+  .mfooter {
+    margin-top: 0!important;
+    ul {
+      li:first-child {
+        display: none;
+      }
+    }
+  }
+</style>
---
+++ app/js/money/mslider.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <Panel title="轮播图" :class="$style.panel">
+    <section :class="$style.content">
+      <Slider :items="items" :cname="$style.slider"></Slider>  
+    </section>
+  </Panel>
+</template>
+
+<script>
+  import Panel from "../core/panel.vue"
+  import Slider from '../core/slider.vue'
+  export default {
+    components: {
+      Panel,
+      Slider
+    },
+    data() {
+      return {
+        items: [
+          {
+            href: "home",
+            src: "//img12.360buyimg.com/jrpmobile/jfs/t13744/11/2508939408/43213/58bfe502/5a421ff3N52d0a474.jpg?width=750&height=320"
+          }, 
+          {
+            href: "home",
+            src: "//img12.360buyimg.com/jrpmobile/jfs/t16138/325/817844900/36681/5d874455/5a422029Ncd678ea3.jpg?width=750&height=320"
+          }, 
+          {
+            href: "home",
+            src: "//img12.360buyimg.com/jrpmobile/jfs/t13465/324/2563794460/43029/3ffca963/5a421f83Ne8089231.jpg?width=750&height=320"
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" module>
+  @import "../../css/element.scss";
+  .panel{
+    @include panel;
+    >h4{
+      display: none;
+    }
+    .content{
+      .slider{
+        margin-top: 120px;
+        img{
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
